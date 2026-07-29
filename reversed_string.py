942310b4cf43f632675062ca40be867f0ebf5afc
DhanushiAkula <dhanushiakula17@gmail.com>
2026-07-29 13:08:40 +0300

    for_loops

---
+++ reversed_string.py
@@ -0,0 +1,6 @@
+string=(input("enter the word:"))
+reversed_string=""
+for i in string:
+    reversed_string=i+reversed_string
+print("the original string:",string)
+print("the reversed string:",reversed_string)
